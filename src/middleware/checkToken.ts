e165a67faa852c14e5fda8aa5851b07bf112eff2
peerapat_vgen <peerapat.vgen@gmail.com>
2023-09-17 21:57:43 +0700

    update middleware 17/9/2566 21.57

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -5,13 +5,17 @@ import { findMemberById } from '../controllers/userController';
 export const checkRequestToken = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const token: string | undefined = Array.isArray(req.headers["token-request"]) ? req.headers["token-request"][0] : req.headers["token-request"];
-
+        console.log(token)
         if (token === undefined) {
             res.status(401).send('No Token, Authorization Denied');
             return;
         }
-
-        const decode = jwt.verify(token, 'jwtSecret') as jwt.JwtPayload;
+        if (!process.env.JWT_SECRET) {
+            res.status(401).json("Error Server : Please restart server");
+            return
+          }
+        const decode = jwt.verify(token, process.env.JWT_SECRET) as jwt.JwtPayload;
+  
         // console.log("middleware" , decode);
         if(decode){
             const checkUser = await findMemberById(decode.id);
